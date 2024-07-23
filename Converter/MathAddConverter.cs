b553e59f9e53c170ec01c02a88e6f30ee177f109
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-23 14:01:08 +0800

    featL: implement MathAddConverter

--- Converter/MathAddConverter.cs
+++ Converter/MathAddConverter.cs
@@ -4,15 +4,21 @@ using System.Globalization;
 
 namespace ValueConversionSample.Converter;
 
-internal class MathAddConverter : IValueConverter
+/// <summary>
+/// This is a converter which will add two numbers
+/// </summary>
+public class MathAddConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        // For add this is simple. just return the sum of the value and the parameter.
+        // You may want to validate value and parameter in a real world App
+        return (decimal?)value + (decimal?)parameter;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        // If we want to convert back, we need to subtract instead of add.
+        return (decimal?)value - (decimal?)parameter;
     }
 }
