de4c53c69d3b88a39a83a927ae5167e55129c7f6
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-23 15:44:12 +0800

    feat: add MathMultiConverter

---
+++ Converter/MathMultiConverter.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ValueConversionSample.Converter;
+
+internal class MathMultiConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,35 @@ namespace ValueConversionSample.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    // The initial value is "+" (Add).
+    private string _operator = "+";
+
     // The initial value is 2.
     private decimal? _number1 = 2;
+
+    // The initial value is 3.
+    private decimal? _number2 = 3;
+
+    /// <summary>
+    /// Gets a collection of available operators
+    /// </summary>
+    public string[] AvailableMathOperators { get; } = ["+", "-", "*", "/"];
+
+    public string Operator
+    {
+        get { return _operator; }
+        set { this.RaiseAndSetIfChanged(ref _operator, value); }
+    }
 
     /// <summary>
     /// This is our Number 1
     /// </summary>
     public decimal? Number1
     { get { return _number1; } set { this.RaiseAndSetIfChanged(ref _number1, value); } }
+
+    /// <summary>
+    /// This is our Number 2
+    /// </summary>
+    public decimal? Number2
+    { get { return _number2; } set { this.RaiseAndSetIfChanged(ref _number2, value); } }
 }
